deeecbf3cf1ebf1aee03c316fdea6dd22a7793e5
Gabriel Augustin <gabriel.augustin_terceiros@nhecotech.com>
2025-07-07 01:38:45 -0300

    chore: Removed unused imports

--- src/main/java/com/augustin/gabriel/goldenraspberryawardsapi/controllers/advices/GlobalExceptionHandler.java
+++ src/main/java/com/augustin/gabriel/goldenraspberryawardsapi/controllers/advices/GlobalExceptionHandler.java
@@ -1,14 +1,12 @@
 package com.augustin.gabriel.goldenraspberryawardsapi.controllers.advices;
 
+import com.augustin.gabriel.goldenraspberryawardsapi.dtos.ErrorResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-
-import com.augustin.gabriel.goldenraspberryawardsapi.dtos.ErrorResponseDto;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @ControllerAdvice
